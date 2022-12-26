7ae4fcd0eec6cbf16818e8ee1e8b4c681a1ced68
FadlanBM <fadlankurahan2@gmail.com>
2022-12-26 16:52:31 +0700

    Belajar Tentang lable

---
+++ Belajar_Table/Belajar_Table/Belajar_Table/Labels.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Belajar_Table
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Labels : ContentPage
+	{
+		public ICommand TapCommand => new Command<string>(OpenBrowser);
+		public Labels()
+		{
+			InitializeComponent();
+			BindingContext=this;
+		}
+
+		void OpenBrowser(string url) {
+			Launcher.OpenAsync(url);
+		}
+	}
+}
